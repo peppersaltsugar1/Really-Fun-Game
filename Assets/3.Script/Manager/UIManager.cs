d52854c703cb378b791cdb053e5fcee4f9ec7071
peppersaltsugar1 <jd9028@naver.com>
2024-08-03 21:48:46 +0900

    2024.08.03 체력바 생성완료 체력바 변동구현중

--- Assets/3.Script/Manager/UIManager.cs
+++ Assets/3.Script/Manager/UIManager.cs
@@ -8,7 +8,8 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Player player;
     [SerializeField]
-    private Dictionary<string, GameObject> hpUI = new();
+    private List<GameObject> hpPrefabsList;
+    private List<GameObject> hpList = new();
     [SerializeField]
     private Canvas canvas;
 
@@ -37,18 +38,74 @@ public class UIManager : MonoBehaviour
     }
     void Start()
     {
-        hpUI.Add("기본체력", GameObject.Find("R_Heart"));
-        hpUI.Add("추가체력", GameObject.Find("RDis_Heart"));
-        hpUI.Add("베터리체력", GameObject.Find("Elect_Heart"));
-        hpUI.Add("쉴드체력", GameObject.Find("Shield_Heart"));
+        HpBarSet();
     }
 
     void Update()
     {
 
     }
+    public void HpBarSet()
+    {
+        int hpNum = 0;
+        if (player.maxHp > 0)
+        {
+            for (int i = 0; i < player.maxHp / 3; i++)
+            {
+                GameObject newHp = Instantiate(hpPrefabsList[0], canvas.transform);
+                newHp.transform.SetParent(canvas.transform,false);
+                RectTransform rectTransform = newHp.GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = new Vector2(i * 115, 0); // 위치 조정 (임의로 설정)
+                hpList.Add(newHp);
+                hpNum += 1;
+            }
+            if (player.temHp > 0)
+            {
+                for (int i = 0; i < player.temHp / 3; i++)
+                {
+                    GameObject newTemHp = Instantiate(hpPrefabsList[1], canvas.transform);
+                    newTemHp.transform.SetParent(canvas.transform, false);
+                    RectTransform rectTransform = newTemHp.GetComponent<RectTransform>();
+                    rectTransform.anchoredPosition = new Vector2(hpNum * 115, 0); // 위치 조정 (임의로 설정)
+                    hpList.Add(newTemHp);
+                    hpNum += 1;
+
+                }
+            }
+
+            if (player.spark > 0)
+            {
+                for (int i = 0; i < player.spark; i++)
+                {
+                    GameObject spark = Instantiate(hpPrefabsList[2], canvas.transform);
+                    spark.transform.SetParent(canvas.transform, false);
+                    RectTransform rectTransform = spark.GetComponent<RectTransform>();
+                    rectTransform.anchoredPosition = new Vector2(hpNum * 115, 0); // 위치 조정 (임의로 설정)
+                    hpList.Add(spark);
+                    hpNum += 1;
+
+                }
+            }
+            if (player.shieldHp > 0)
+            {
+                for (int i = 0; i < player.shieldHp; i++)
+                {
+                    GameObject newShildHp = Instantiate(hpPrefabsList[3], canvas.transform);
+                    newShildHp.transform.SetParent(canvas.transform, false);
+                    RectTransform rectTransform = newShildHp.GetComponent<RectTransform>();
+                    rectTransform.anchoredPosition = new Vector2(hpNum * 115, 0); // 위치 조정 (임의로 설정)
+                    hpList.Add(newShildHp);
+                    hpNum += 1;
+
+                }
+            }
+        }
+    }
     public void HpSet()
     {
-       
+        if (player.shieldHp > 0)
+        {
+
+        }
     }
 }
