a962a28ad67f62f83fd00b7d07067d7e894bfa9b
peppersaltsugar1 <jd9028@naver.com>
2024-08-29 15:00:48 +0900

    2024.08.29 맵분류

---
+++ Assets/3.Script/Map/Map.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public enum MapType { Start, Boss, Special, RandomSpecial, Hidden, Middle, End }
+
+    public MapType Type;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
