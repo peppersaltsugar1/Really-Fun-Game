546126e8d62782f4f75bc805c9a2e849e587bb9e
peppersaltsugar1 <jd9028@naver.com>
2024-10-04 11:47:03 +0900

    맵갱신

--- Assets/3.Script/Map/Map.cs
+++ Assets/3.Script/Map/Map.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    public enum MapType { Start, Boss, Shop, Download, RandomSpecial, Hidden, Middle, End, MiddleBoss }
+    public enum MapType { Start, Boss, Shop,Download, RandomSpecial, Hidden, Middle, End, MiddleBoss }
     public MapType Type;
     public int PortalNum;
     public string mapName;
@@ -24,6 +24,6 @@ public class Map : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
